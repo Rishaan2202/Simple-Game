91366f6158dd45f9f519f940396cb058d4e13262
Rishaan <rishaan.sharma2202@gmail.com>
2026-03-16 17:00:08 +0530

    Added the shop and some JS stuff

--- script.js
+++ script.js
@@ -19,3 +19,43 @@ function resetGame(){
 }
 
 resetButton.addEventListener('click', resetGame);
+
+
+const shopItems = [
+    {
+        name: "Money",
+        description: "Get money with money! +₹5 shipping charges",
+        cost: 15,
+        startingCost: 15,
+    },
+
+    {
+        name: "iPad",
+        description: "Pls gimme an iPad!!!",
+        cost: 100,
+        startingCost: 100,
+    }
+]
+
+function createShopItems() {
+    document.querySelectorAll('.shop-item').forEach((element) => {
+        element.remove();
+});
+}
+
+shopItems.forEach((item) => {
+    const shopItem = document.createElement ('div');
+    shopItem.className = "shop-item";
+
+    shopItem.innerHTML = `
+        <div>
+        <h3>₹{item.name}</h3>
+        <p>₹{item.description}</p>
+        </div>
+        <button onclick="buyItem('₹(item.name)')">
+            Buy for ₹{item.cost}
+        </button>
+    `;
+
+    shopContainer.appendChild(shopItem);
+});
